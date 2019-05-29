23050f01f88c9c360a449f024867d2661ef3e172
Jim Hough <teachlife@outlook.com>
2019-05-29 12:44:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,10 +28,14 @@ class Student
   end
 
   def save
-    sql = 'INSERT INTO students (name, grade) VALUES (?, ?)'
+    if self.id
+      self.update
+    else
+      sql = 'INSERT INTO students (name, grade) VALUES (?, ?)'
 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+    end
   end
 
   def update
